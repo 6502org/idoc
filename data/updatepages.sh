26293f00212e0be328eedffb0943c857fab96c6c
Mike Naberezny <mike@naberezny.com>
2022-11-04 16:03:33 -0700

    Show symbolic links created

--- data/updatepages.sh
+++ data/updatepages.sh
@@ -28,13 +28,13 @@ for language in sv en de fi es hu; do
 done
 
 echo -n "links: "
-ln -sf index.en.html ../html/${version}/index.html
+ln -sfv index.en.html ../html/${version}/index.html
 for page in people intro policy search; do
-  ln -sf ${page}.en.html ../html/${version}/${page}.html
+  ln -sfv ${page}.en.html ../html/${version}/${page}.html
 done
 for table in gm hw pr vc c2 ms langsv langde langhu langfr all; do
   for sorting in none file name; do
-    ln -sf etexts-${table}-${sorting}.en.html ../html/${version}/etexts-${table}-${sorting}.html
+    ln -sfv etexts-${table}-${sorting}.en.html ../html/${version}/etexts-${table}-${sorting}.html
   done
 done
 echo
